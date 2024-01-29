04bf941ce6ec37b1f15ab46338b4b598c8795338
Adam <abaviles@ucsc.edu>
2024-01-28 23:02:05 -0800

    Final Game & Mod Explanation

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-//Adam Aviles - "Rocket Patrol: Return of the King Endgame IV (& Knuckles)"
+/*Adam Aviles - "Rocket Patrol: Return of the King Endgame IV (& Knuckles)"
+- ~8 hours to complete
+
+Mods: 
+- High score tracker 1
+- "FIRE!" UI 1
+- (copyright free) background music 1
+- speed increase after time interval 1
+- post-fire player control 1
+
+- new tile background & parallax (i hand drew the clouds :D) 1 + 3 (?)
+- timer display 3
+- new title screen 3
+
+- new timing/scoring mechanism 5
+
+RESOURCES: 
+parallax tutorial: https://www.joshmorony.com/how-to-create-a-parallax-background-in-phaser/
+music: my roomate Brian Law
+additional support: my friend Benthan Vu
+*/
 let config = {
     type: Phaser.AUTO,
     width: 1280,
